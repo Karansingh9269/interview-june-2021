e025ff7d4f5acc86eebd577d29553d8213659e6c
Karansingh9269 <86247862+Karansingh9269@users.noreply.github.com>
2021-06-22 12:18:31 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -4,6 +4,22 @@ public class ConvertToBinary {
 
   public String toBinary(final int n) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-    return "";
+    //size of interger assume as a 32 biy
+    for (int i=31;i>=0;i--)
+    {
+      int k=n>>i;
+      if ((k & 1)>0)
+          System.out.println("1");
+      else
+          System.out.println("0");
+    }
   }
+}
+class karan
+  public static void main(String[] args)
+{
+  ConvertToBinary c=new ConvertToBinary();
+  int n=32;
+  g.toBinary(n);
+}
 }
